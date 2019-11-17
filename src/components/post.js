fcf811111350c2e57dcdda95816c52cf673bf412
socraticDevBlog <maximebonin@outlook.com>
2019-11-17 09:41:10 -0500

    replace par by by

--- src/components/post.js
+++ src/components/post.js
@@ -22,7 +22,7 @@ const Post = ({ title, author, slug, date, body, fluid, tags }) => {
           <Link to={slug}>{title}</Link>
         </CardTitle>
         <CardSubtitle>
-          <span className="text-info">{date}</span> par{" "}
+          <span className="text-info">{date}</span> by{" "}
           <span className="text-info">{author}</span>
         </CardSubtitle>
         <CardText>{body}</CardText>
