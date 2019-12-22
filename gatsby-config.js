55d351213a4370df5a9331734e47fe1d86c17400
socraticDevBlog <maximebonin@outlook.com>
2019-12-22 15:44:25 -0500

    google analytic added

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,17 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-154676737-2`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
