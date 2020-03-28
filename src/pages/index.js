2295883398b2312eb73d321a693d3b69c059c873
socraticDevBlog <maximebonin@outlook.com>
2020-03-27 23:08:33 -0400

    added pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,42 +3,66 @@ import { graphql, StaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Post from "../components/post"
+import PageLinks from "../components/page-links"
 
-const IndexPage = () => (
-  <Layout pageTitle= "">
-    <SEO title="Main" keywords={["tech blog", "Maxime Bonin", "dev", "developer", "programming", "technology","software engineering"]} />
-    <StaticQuery
-      query={indexQuery}
-      render={data => {
-        return (
-          <div>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-              <Post
-                key={node.id}
-                title={node.frontmatter.title}
-                author={node.frontmatter.author}
-                slug={node.fields.slug}
-                date={node.frontmatter.date}
-                body={node.excerpt}
-                fluid={node.frontmatter.image.childImageSharp.fluid}
-                tags={node.frontmatter.tags}
-              />
-            ))}
-          </div>
-        )
-      }}
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  const postsPerPage = 5
+  let numberOfPages
+  return (
+    <Layout pageTitle="">
+      <SEO
+        title="Accueil"
+        keywords={[
+          "Maxime Bonin",
+          "developer",
+          "fullstack",
+          "software engineering",
+          "blogue techno",
+          "socraticdev",
+          "dailyprog",
+        ]}
+      />
+      <StaticQuery
+        query={indexQuery}
+        render={data => {
+          numberOfPages = Math.ceil(
+            data.allMarkdownRemark.totalCount / postsPerPage
+          )
+          return (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Post
+                  key={node.id}
+                  title={node.frontmatter.title}
+                  author={node.frontmatter.author}
+                  slug={node.fields.slug}
+                  date={node.frontmatter.date}
+                  body={node.excerpt}
+                  fluid={node.frontmatter.image.childImageSharp.fluid}
+                  tags={node.frontmatter.tags}
+                />
+              ))}
+              <PageLinks currentPage={1} numberOfPages={numberOfPages} />
+            </div>
+          )
+        }}
+      />
+    </Layout>
+  )
+}
 
 const indexQuery = graphql`
   query MyQuery {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 5
+    ) {
+      totalCount
       edges {
         node {
           frontmatter {
             author
-            date(formatString: "dddd MMMM Do YYYY", locale: "en")
+            date(formatString: "dddd Do MMMM YYYY", locale: "fr")
             title
             tags
             image {
