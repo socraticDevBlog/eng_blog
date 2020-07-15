bab8e56038589f04b3291c0cf31b81a13e3bca2e
socraticDev <maximebonin@outlook.com>
2020-07-14 20:53:26 -0400

    adding head favicon svg

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types"
 import React from "react"
+import Logo from "../images/head.svg"
 
 import {
   Collapse,
@@ -29,29 +30,55 @@ class Header extends React.Component {
     return (
       <Navbar fixed="top" light expand="sm">
         <div className="container">
-          <NavbarBrand href="/" title="Home">{this.props.siteTitle}</NavbarBrand>
+          <NavbarBrand href="/" title="Home">
+            <img
+              class="mr-2"
+              src={Logo}
+              width="35"
+              height="35"
+              alt="head logo"
+            />
+            {this.props.siteTitle}
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/about" title="About">About</NavLink>
+                <NavLink href="/about" title="About">
+                  About
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/tags" title="Topics">Topics</NavLink>
+                <NavLink href="/tags" title="Topics">
+                  Topics
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/disclaimer" title="Disclaimer">Disclaimer</NavLink>
+                <NavLink href="/disclaimer" title="Disclaimer">
+                  Disclaimer
+                </NavLink>
               </NavItem>
               <NavItem>
                 <NavLink title="subscribe to RSS Feed" href="/rss.xml">
                   RSS
-                  <span aria-label="subscription to RSS feed" role="img">💌 </span>
+                  <span aria-label="subscription to RSS feed" role="img">
+                    💌{" "}
+                  </span>
                 </NavLink>
               </NavItem>
 
               <NavItem>
-                <NavLink href="https://socratic.dev" title="Vers le site en français"> 
-                  <span aria-label="visit site in french" title="Vers le site en français" role="img">🇫🇷</span>
+                <NavLink
+                  href="https://socratic.dev"
+                  title="Vers le site en français"
+                >
+                  <span
+                    aria-label="visit site in french"
+                    title="Vers le site en français"
+                    role="img"
+                  >
+                    🇫🇷
+                  </span>
                 </NavLink>
               </NavItem>
             </Nav>
