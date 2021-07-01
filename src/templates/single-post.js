b7c294d424e4b331ead2a282bfbe0e5617837bac
socraticDev <maximebonin@outlook.com>
2021-07-01 15:27:24 -0400

    social media preview meta tags

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -6,11 +6,52 @@ import { Card, CardSubtitle, CardBody, Badge } from "reactstrap"
 import Img from "gatsby-image"
 import { slugify } from "../util/util-functions"
 
+import { Helmet } from "react-helmet"
+
+const rootUrl = `{rootUrlImg}/`
+const rootUrlImg = "https://en.socratic.dev"
+const siteTitle = "socraticDev's Blog"
+
 const SinglePost = ({ data }) => {
   const post = data.markdownRemark.frontmatter
   return (
     <Layout pageTitle={post.title}>
       <SEO title={post.title} />
+
+      <Helmet>
+        <title>{`${post.title} | ${siteTitle}`}</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content={`${post.title}`} />
+        <meta
+          property="og:description"
+          content={`${data.markdownRemark.excerpt}`}
+        />
+        <meta
+          property="og:image"
+          content={`${rootUrlImg}${post.image.childImageSharp.fluid.src}/`}
+        />
+        <meta
+          property="og:url"
+          content={`${rootUrl}${data.markdownRemark.fields.slug}/`}
+        />
+        <meta
+          property="twitter:image"
+          content={`${rootUrlImg}${post.image.childImageSharp.fluid.src}`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:description"
+          content={`${data.markdownRemark.excerpt}`}
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:locale" content="fr_CA" />
+        <link
+          rel="canonical"
+          href={`${rootUrl}${data.markdownRemark.fields.slug}/`}
+        />
+      </Helmet>
+
       <Card>
         <Img
           className="card-image-top"
@@ -18,7 +59,7 @@ const SinglePost = ({ data }) => {
         />
         <CardBody>
           <p>
-            <span className="text-info">{post.date}</span> by {' '}
+            <span className="text-info">{post.date}</span> by{" "}
             <span className="text-info">{post.author}</span>
           </p>
           <CardSubtitle>
@@ -26,10 +67,12 @@ const SinglePost = ({ data }) => {
               dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
             />
             <ul className="post-tags">
-              {post.tags.map(tag => (
+              {post.tags.map((tag) => (
                 <li key={tag}>
                   <Link to={`/tag/${slugify(tag)}`}>
-                    <Badge pill color="primary">{tag}</Badge>
+                    <Badge pill color="primary">
+                      {tag}
+                    </Badge>
                   </Link>
                 </li>
               ))}
@@ -57,6 +100,7 @@ export const postQuery = graphql`
         image {
           childImageSharp {
             fluid(maxHeight: 200, maxWidth: 600) {
+              src
               ...GatsbyImageSharpFluid
             }
           }
