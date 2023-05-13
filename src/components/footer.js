2df14b0b0cd3d431cd32e933e7a5101258360dad
Maxime Bonin <maximebonin@outlook.com>
2023-05-12 23:06:34 -0400

    new post pareto and improve design

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,22 @@
 import React from "react"
 import TwitterLogo from "../images/twitter.png"
-import LinkedinLogo from "../images/linkedin.png"
 import GithubLogo from "../images/github.png"
 import GitlabLogo from "../images/gitlab.png"
-import MaxHomepageLogo from "../images/homepage.png"
+import MastadonLogo from "../images/mastadon48x48.png"
 
 const Footer = () => (
   <div className="site-footer">
-    <p>SocraticDev elsewhere on the web</p>
+    <p>SocraticDev ailleurs sur le web</p>
     <div className="footer-social-links">
       <ul className="social-links-list">
-        <li>
+        <li className="">
           <a
             className=""
             title="https://github.com/socraticDevBlog/"
             href="https://github.com/socraticDevBlog/"
             rel="noopener noreferrer"
             target="_blank"
-            alt="url to my Github page"
+            alt="lien vers ma page github"
           >
             <img
               src={GithubLogo}
@@ -26,14 +25,14 @@ const Footer = () => (
             ></img>
           </a>
         </li>
-        <li>
+        <li className="">
           <a
             className=""
             title="https://gitlab.com/socraticdev/"
             href="https://gitlab.com/socraticdev/"
             rel="noopener noreferrer"
             target="_blank"
-            alt="url to my gitLab page"
+            alt="lien vers ma page gitLab"
           >
             <img
               src={GitlabLogo}
@@ -45,47 +44,31 @@ const Footer = () => (
         <li>
           <a
             className=""
-            title="https://www.linkedin.com/in/boninmaxime/?locale=en_US"
-            href="https://www.linkedin.com/in/boninmaxime/?locale=en_US"
+            title="https://mastodon.social/@socdev"
+            href="https://mastodon.social/@socdev"
             rel="noopener noreferrer"
             target="_blank"
-            alt="url to my LinkedIn page"
+            alt="lien vers ma page mastadon"
           >
             <img
-              src={LinkedinLogo}
-              alt="linkedIn logo"
-              className="google"
-            ></img>
-          </a>
-        </li>
-        <li>
-          <a
-            className=""
-            title="https://twitter.com/socraticdevblog"
-            href="https://twitter.com/socraticdevblog"
-            rel="noopener noreferrer"
-            target="_blank"
-            alt="url to my Twitter page"
-          >
-            <img
-              src={TwitterLogo}
-              alt="twitter logo"
+              src={MastadonLogo}
+              alt="mastadon logo"
               className="socialMediaLogo"
             ></img>
           </a>
         </li>
         <li>
           <a
-            className=""
-            title="https://en.maximebonin.dev"
-            href="https://en.maximebonin.dev"
+            className="twitter"
+            title="https://twitter.com/socraticdevblog"
+            href="https://twitter.com/socraticdevblog"
             rel="noopener noreferrer"
             target="_blank"
-            alt="url to my personal homepage"
+            alt="lien vers ma page Twitter"
           >
             <img
-              src={MaxHomepageLogo}
-              alt="Max Homepage logo"
+              src={TwitterLogo}
+              alt="twitter logo"
               className="socialMediaLogo"
             ></img>
           </a>
