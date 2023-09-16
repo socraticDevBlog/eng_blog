bd2d69d9e4aaf1c7aec282508c8db0376210f99a
socraticDevBlog <maximebonin@outlook.com>
2023-09-15 21:33:29 -0400

    purify styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children, pageTitle }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div className="container" id="content">
         <img id="main_logo" class="center" src={ Logo } alt="SocraticDev logo"></img>
-        <h3>{pageTitle}</h3>
+        <h2>{pageTitle}</h2>
         <Row>
           <Col md="2"></Col>
           <Col md="8">
