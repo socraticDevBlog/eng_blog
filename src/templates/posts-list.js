9ddaf531d9ed4aa799b6f5f92888c35a42a2af0f
Maxime Bonin <maximebonin@outlook.com>
2025-10-28 20:21:08 -0400

    add social media preview for landing page

--- src/templates/posts-list.js
+++ src/templates/posts-list.js
@@ -3,13 +3,42 @@ import Layout from "../components/layout"
 import Post from "../components/post"
 import { graphql } from "gatsby"
 import PageLinks from "../components/page-links"
+import bannerImage from "../images/_banner.svg"
 
-const postsList = props => {
+import { Helmet } from "react-helmet"
+const rootUrl = "https://en.socratic.dev"
+const rootUrlImg = "https://en.socratic.dev"
+const siteTitle = "socraticDev's Blog"
+const author = "socraticDev"
+const description = "a blog about technology and philosophy"
+const bannerImageUrl = `${rootUrlImg}${bannerImage}`
+
+const postsList = (props) => {
   const posts = props.data.allMarkdownRemark.edges
   const { currentPage, pagesCount } = props.pageContext
 
   return (
-    <Layout pageTitle={ currentPage }>
+    <Layout pageTitle={currentPage}>
+      <Helmet>
+        <title>{`${siteTitle}`}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:author" content={author} />
+        <meta property="og:type" content="article" />
+        <meta property="article:publisher" content={rootUrl} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={bannerImageUrl} />
+        <meta property="og:url" content={`${rootUrl}`} />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta property="twitter:image" content={bannerImageUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content={description} />
+        <meta property="og:type" content="article" />
+        <meta property="og:locale" content="en_CA" />
+        <link rel="canonical" href={`${rootUrl}`} />
+      </Helmet>
+
       {posts.map(({ node }) => (
         <Post
           key={node.id}
@@ -22,7 +51,7 @@ const postsList = props => {
           fluid={node.frontmatter.image.childImageSharp.gatsbyImageData}
         />
       ))}
-      <PageLinks currentPage={currentPage} numberOfPages={pagesCount}/>
+      <PageLinks currentPage={currentPage} numberOfPages={pagesCount} />
     </Layout>
   )
 }
