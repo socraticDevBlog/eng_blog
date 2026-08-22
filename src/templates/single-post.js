e177cd841fdec7fd65c985d935ec6742883edf6a
socraticDevBlog <hello@socratic.dev>
2026-08-22 11:59:32 -0400

    fix og

--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -1,57 +1,50 @@
 import React from "react"
 import Layout from "../components/layout"
 import { graphql, Link } from "gatsby"
-import SEO from "../components/seo"
 import { Card, CardTitle, CardSubtitle, CardBody, Badge } from "reactstrap"
 import { GatsbyImage, getImage, getSrc } from "gatsby-plugin-image"
 import { slugify } from "../util/util-functions"
-
-import { Helmet } from "react-helmet"
 
 const rootUrl = "https://en.socratic.dev"
 const rootUrlImg = "https://en.socratic.dev"
 const siteTitle = "socraticDev's Blog"
 
-const SinglePost = ({ data }) => {
+export const Head = ({ data }) => {
   const post = data.markdownRemark.frontmatter
-  const img = getImage(post.image.childImageSharp.gatsbyImageData)
-  const imgSrc = getSrc(img)
   const socialImg = getImage(post.socialImage.childImageSharp.gatsbyImageData)
   const socialImgSrc = getSrc(socialImg)
+  const seoTitle = post.seoTitle || post.title
+  const seoDescription = post.seoDescription || data.markdownRemark.excerpt
+  const canonicalUrl = `${rootUrl}/${data.markdownRemark.fields.slug}`
+
+  return (
+    <>
+      <html lang="en" />
+      <title>{`${seoTitle} | ${siteTitle}`}</title>
+      <meta name="description" content={seoDescription} />
+      <meta property="og:title" content={seoTitle} />
+      <meta property="og:author" content={post.author} />
+      <meta property="og:type" content="article" />
+      <meta property="article:publisher" content={rootUrl} />
+      <meta property="og:description" content={seoDescription} />
+      <meta property="og:image" content={`${rootUrlImg}${socialImgSrc}`} />
+      <meta property="article:published_time" content={post.dateISO} />
+      <meta property="og:url" content={canonicalUrl} />
+      <meta property="og:site_name" content={siteTitle} />
+      <meta name="twitter:image" content={`${rootUrlImg}${socialImgSrc}`} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seoTitle} />
+      <meta name="twitter:description" content={seoDescription} />
+      <meta property="og:locale" content="en_CA" />
+      <link rel="canonical" href={canonicalUrl} />
+    </>
+  )
+}
+
+const SinglePost = ({ data }) => {
+  const post = data.markdownRemark.frontmatter
   return (
     <Layout pageTitle={""}>
-      <SEO title={post.title} />
-
-      <Helmet>
-        <title>{`${post.title} | ${siteTitle}`}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content={post.title} />
-        <meta property="og:author" content={post.author} />
-        <meta property="og:type" content="article" />
-        <meta property="article:publisher" content="https://en.socratic.dev" />
-        <meta property="og:description" content={data.markdownRemark.excerpt} />
-        <meta property="og:image" content={`${rootUrlImg}${socialImgSrc}`} />
-        <meta property="article:published_time" content={post.dateISO} />
-        <meta
-          property="og:url"
-          content={`${rootUrl}/${data.markdownRemark.fields.slug}`}
-        />
-        <meta property="og:site_name" content={siteTitle} />
-        <meta property="twitter:image" content={`${rootUrlImg}${imgSrc}`} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:description"
-          content={data.markdownRemark.excerpt}
-        />
-        <meta property="og:type" content="article" />
-        <meta property="og:locale" content="en_CA" />
-        <link
-          rel="canonical"
-          href={`${rootUrl}/${data.markdownRemark.fields.slug}`}
-        />
-      </Helmet>
-
       <Card>
         <GatsbyImage
           className="card-image-top"
